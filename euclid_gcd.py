31f156f4b8a1e93329ef7632bc3e659a62a17992
ccastro <caiofcastro@usp.br>
2022-06-08 18:51:58 -0300

    [step 1] implement basic algo

--- euclid_gcd.py
+++ euclid_gcd.py
@@ -1,9 +1,19 @@
 def euclid_gcd(n1, n2):
-    return 1
+    if n1 > n2:
+        a, b = n1, n2
+    else:
+        a, b = n2, n1
+
+    while (b != 0):
+        old_b = b
+        b = a % b
+        a = old_b
+
+    return a
 
 
 if __name__ == "__main__":
     import sys
+
     n1, n2 = sys.argv[1:]
-    
     gcd = euclid_gcd(n1, n2)
